6995f5e804ca17acc4262955d41b60e118be9289
Maulashree <66380792+Maulashree@users.noreply.github.com>
2023-02-24 14:44:57 -0500

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,124 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import Board, { moveCard } from "@lourenci/react-kanban";
+import "@lourenci/react-kanban/dist/styles.css";
+
+const board = {
+  columns: [
+    {
+      id: 1,
+      title: "To Do",
+      backgroundColor: "#fff",
+      cards: [
+        {
+          id: 1,
+          title: "Card title 1",
+          description: "Card content"
+        },
+        {
+          id: 2,
+          title: "Card title 2",
+          description: "Card content"
+        },
+        {
+          id: 3,
+          title: "Card title 3",
+          description: "Card content"
+        }
+      ]
+    },
+    {
+      id: 2,
+      title: "In Progress",
+      cards: [
+        {
+          id: 9,
+          title: "Card title 9",
+          description: "Card content"
+        }
+      ]
+    },
+    {
+      id: 3,
+      title: "Ready To Rivew",
+      cards: [
+        {
+          id: 10,
+          title: "Card title 10",
+          description: "Card content"
+        },
+        {
+          id: 11,
+          title: "Card title 11",
+          description: "Card content"
+        }
+      ]
+    },
+    {
+      id: 4,
+      title: "Finished",
+      cards: [
+        {
+          id: 12,
+          title: "Card title 12",
+          description: "Card content"
+        },
+        {
+          id: 13,
+          title: "Card title 13",
+          description: "Card content"
+        }
+      ]
+    }
+  ]
+};
+
+function ControlledBoard() {
+  // You need to control the state yourself.
+  const [controlledBoard, setBoard] = useState(board);
+
+  function handleCardMove(_card, source, destination) {
+    const updatedBoard = moveCard(controlledBoard, source, destination);
+    setBoard(updatedBoard);
+  }
+
+  return (
+    <Board onCardDragEnd={handleCardMove} disableColumnDrag>
+      {controlledBoard}
+    </Board>
+  );
+}
+
+function UncontrolledBoard() {
+  return (
+    <Board
+      allowRemoveLane
+      allowRenameColumn
+      allowRemoveCard
+      onLaneRemove={console.log}
+      onCardRemove={console.log}
+      onLaneRename={console.log}
+      initialBoard={board}
+      allowAddCard={{ on: "top" }}
+      onNewCardConfirm={(draftCard) => ({
+        id: new Date().getTime(),
+        ...draftCard
+      })}
+      onCardNew={console.log}
+    />
+  );
+}
+
+function App() {
+  return (
+    <>
+      <h4>Example of an Kanban Board</h4>
+      <UncontrolledBoard />
+     
+      <ControlledBoard />
+    </>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
